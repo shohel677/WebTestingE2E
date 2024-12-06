9cfd2ffd22e023ac2518cf12445b5c5709f081f8
shohel677 <shohel877@gmail.com>
2024-12-07 01:38:10 +0600

    StandAlone test java update

--- Test/src/test/java/tests/StandAloneTest.java
+++ Test/src/test/java/tests/StandAloneTest.java
@@ -19,33 +19,6 @@ public class StandAloneTest {
             WebDriver driver = new ChromeDriver();
             driver.manage().window().maximize();
 
-            try {
-                // Navigate to the target URL
-                driver.get("https://vanity-qa02-stage01-gtn03-superset.qa.gtn.oa.iqvia.com/superset/sqllab/");
-
-                Thread.sleep(60000);
-                // Locate the hidden textarea within the ACE editor
-                WebElement textarea = driver.findElement(By.cssSelector("#ace-editor .ace_text-input"));
-
-                // Input text using JavaScript Executor
-                String inputText = "Your input text";
-                JavascriptExecutor js = (JavascriptExecutor) driver;
-                js.executeScript("arguments[0].value = arguments[1];", textarea, inputText);
-
-                // Dispatch an 'input' event to update the ACE editor's state
-                js.executeScript("arguments[0].dispatchEvent(new Event('input', { bubbles: true }));", textarea);
-
-                // Optionally validate if the text is updated (depends on the page logic)
-                System.out.println("Text input applied successfully.");
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                // Close the browser
-               // driver.quit();
-            }
-
-
 
     }
 }
